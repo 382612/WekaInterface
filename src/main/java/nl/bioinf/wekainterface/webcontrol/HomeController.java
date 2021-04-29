83bd1eee8841265e6683d49be42e2721fdd1b36c
Marijke Eggink <Marijke@MacBook-Air-van-Marijke-2.local>
2021-04-29 13:56:47 +0200

    Method created that lists filenames

--- src/main/java/nl/bioinf/wekainterface/webcontrol/HomeController.java
+++ src/main/java/nl/bioinf/wekainterface/webcontrol/HomeController.java
@@ -3,6 +3,10 @@ package nl.bioinf.wekainterface.webcontrol;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * @author Marijke Eggink
+ */
+
 @Controller
 public class HomeController {
     @GetMapping(value = "/home")
