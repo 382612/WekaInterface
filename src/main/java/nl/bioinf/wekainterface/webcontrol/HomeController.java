ac471d4d528628ee8abcc6388bfbd43127630c27
Marijke Eggink <Marijke@MacBook-Air-van-Marijke-2.local>
2021-05-05 16:06:23 +0200

    started explorer page

--- src/main/java/nl/bioinf/wekainterface/webcontrol/HomeController.java
+++ src/main/java/nl/bioinf/wekainterface/webcontrol/HomeController.java
@@ -1,7 +1,11 @@
 package nl.bioinf.wekainterface.webcontrol;
 
+import nl.bioinf.wekainterface.model.DataReader;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * @author Marijke Eggink
@@ -17,6 +21,14 @@ public class HomeController {
     @GetMapping(value = "/Information")
     public String getInfoPage(){
         return "infoPage";
+    }
+
+    @GetMapping(value = "/explorer")
+    public String getWekaExplorerPage(Model model){
+        DataReader reader = new DataReader();
+        List<String> filenames = reader.getDataSetNames();
+        model.addAttribute("filenames", filenames);
+        return "wekaExplorerPage";
     }
 
 }
