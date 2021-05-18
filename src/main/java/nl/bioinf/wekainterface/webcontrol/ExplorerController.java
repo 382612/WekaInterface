e33a65db653f0d2ec66b6dfe7f90580b212ab67e
Jelle <j.d.becirspahic@st.hanze.nl>
2021-05-18 11:38:34 +0200

    Created controller for more complicated requests and move plotData function from HomeController to ExplorerController

--- src/main/java/nl/bioinf/wekainterface/webcontrol/ExplorerController.java
+++ src/main/java/nl/bioinf/wekainterface/webcontrol/ExplorerController.java
@@ -1,11 +1,13 @@
 package nl.bioinf.wekainterface.webcontrol;
 
 import nl.bioinf.wekainterface.model.DataReader;
+import nl.bioinf.wekainterface.model.LabelCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -19,5 +21,18 @@ public class ExplorerController {
         List<String> filenames = dataReader.getDataSetNames();
         model.addAttribute("filenames", filenames);
         return "wekaExplorerPage";
+    }
+
+    @GetMapping(value = "/test")
+    public String plotWeatherData(Model model) throws IOException {
+        String file = "/Users/Marijke/wekafiles/data/weather.nominal.arff";
+        LabelCounter labelCounter = new LabelCounter();
+        labelCounter.readData(file);
+        labelCounter.setGroups();
+        labelCounter.countLabels();
+        model.addAttribute("data", labelCounter.mapToJSON());
+        model.addAttribute("attributes", labelCounter.getAttributeArray());
+        model.addAttribute("classLabel", labelCounter.getClassLabel());
+        return "dataExplorer";
     }
 }
